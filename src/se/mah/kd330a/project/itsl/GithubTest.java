bf914144cbfca60b86e08c29215e1a1e0908607d
Erik <erikhassel94@gmail.com>
2014-05-07 15:09:58 +0200

    å och ö funkar nu

--- src/se/mah/kd330a/project/itsl/GithubTest.java
+++ src/se/mah/kd330a/project/itsl/GithubTest.java
@@ -2,7 +2,7 @@ package se.mah.kd330a.project.itsl;
 
 public class GithubTest {
 
-	// Testa s� ditt Github fungerar, skriv ditt namn nedanf�r och commit/pusha det.
+	// Testa så ditt Github fungerar, skriv ditt namn nedanför och commit/pusha det.
 	// David
 	//Erik
 	
