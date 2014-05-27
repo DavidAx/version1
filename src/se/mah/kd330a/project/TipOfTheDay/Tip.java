e5c54f79de2f584faa02f509c495de55faf0a72a
tor <tor-moller@hotmail.com>
2014-05-27 12:05:34 +0200

    Updaterade Tip

--- src/se/mah/kd330a/project/TipOfTheDay/Tip.java
+++ src/se/mah/kd330a/project/TipOfTheDay/Tip.java
@@ -4,27 +4,12 @@ package se.mah.kd330a.project.TipOfTheDay;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
-import org.mcsoxford.rss.RSSItem;
 
-/*
- * @author asampe
- * 
- * @author marcusmansson
- * 
- * this is a wrapper class for RSSItem, adding features such as making the
- * articles sortable, and method for getting a summary text for large content
- * 
- * version history:
- * 0.7.2 added Serializable, changed constructor // marcusmansson
- * 
- */
 public class Tip implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	private static final int maxSummaryLength = 80;
-	private transient RSSItem rssItem;
 	private boolean textVisible;
-	private String articleCourseCode;
 	private String header;
 	private String date;
 	private String text;
